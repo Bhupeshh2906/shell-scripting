207d13c3ae9f3f82f7d07804fc85151301e3a419
Bhupesh Gurramkonda <bhupeshh310@gmail.com>
2026-03-06 15:59:50 +0530

    array script is added

---
+++ 07-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo " Blockbuster Movies"
+Movies=("HIT" "HIT2" "HIT3")
+
+echo "List movies that are playing at Blockbuster Movies are ${Movies[@]}"
+
+echo " Movie playing in Screen1 is ${Movies[0]}"
+
+echo " Movie playing in Screen2 is ${Movies[1]}"
+
+echo " Movie playing in Screen3 is ${Movies[2]}"
