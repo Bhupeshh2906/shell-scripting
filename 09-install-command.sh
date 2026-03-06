ac3780dd599ad4f5da83bf8f8418c5506af2149a
Bhupesh Gurramkonda <bhupeshh310@gmail.com>
2026-03-06 17:27:03 +0530

    installation script is added

---
+++ 09-install-command.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+UserID=$(id -u)
+
+if [ $UserID -ne 0 ]
+then {
+    echo "Error: You are running the command in no root user."
+    exit 1
+}
+else {
+    echo "Success: you are running the command in root user."
+}
+fi
+
+dnf installed nginx 
+if [ $? -ne 0 ]
+
+then {
+   echo " nginx is not installed. Now going to install."
+   dnf install nginx -y
+
+   if [ $? -eq 0]
+
+   then {
+    echo "nginx has been installed successfully."
+   }
+   else {
+    echo "nginx is not installed .... failure."
+    exit 1
+   }
+   fi
+}
+else {
+    echo "nginx is already installed nothing to do."
+}
+fi 
