459795a565273e15a5ac08ee2cc39647a7240033
Bhupesh Gurramkonda <bhupeshh310@gmail.com>
2026-03-30 12:15:43 +0530

    roboshop services servers creation script

---
+++ roboshop/roboshop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0e7162c29a2e32f28"
+INSTANCES=("Mongodb" "Redis" "Mysql" "rabbitmq" "catalogue" "shipping" "user" "cart" "payment" "dispatch" "frontend")
+ZONE_ID="Z06617789DHVGKOPMYRA"
+DOMAIN_NAME="projectprac.fun"
+
+for instance in "${INSTANCES[@]}"
+do
+INSTANCE_ID=$(aws ec2 run-instances \
+            --region us-east-1 \
+            --image-id $AMI_ID \
+            --instance-type t3.micro \
+            --security-group-ids $SG_ID \
+            --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+            --query "Instances[0].InstanceId" \
+            --output text)
+
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text) 
+        echo " instance $instance IP address is $IP."
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+        echo " $instance ip address is $IP."   
+    fi
+done
